83346ccf54837f503ab47f1cc0eaa226d508c35c
ikyyybitzzxx <106673608+ikyyybitzzxx@users.noreply.github.com>
2022-07-10 16:20:17 +0700

    Update config.js

--- config.js
+++ config.js
@@ -21,7 +21,7 @@ global.gopay = '085922480867'
 
 //=============『 Info Bot 』=================//
 global.namebot = 'Chruz-Xd'
-global.gc = 'https://chat.whatsapp.com/FkdfDk3foMFINwFQKJogWj'
+global.gc = 'https://chat.whatsapp.com/Gx2HsxHu611J01BKNsdYyH'
 global.web = 'https://chat.whatsapp.com/FkdfDk3foMFINwFQKJogWj' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
 global.webm = 'https://saweria.co/arullOfc'
 global.price1 = '𝐍𝐈𝐇 𝐁𝐀𝐍𝐆 𝐋𝐈𝐒𝐓 𝐒𝐄𝐖𝐀𝐍𝐘𝐀\n\n》1 MINGGU Rp 5.000\nMAKSIMAL 1 GRUP\n》1 BULAN  Rp 10.000\nMAKSIMAL 1 GRUP \n》 Rp 20.000\nMAKSIMAL 1 GRUP + PREMIUM\n》PERMANEN Rp 30.000\nMAKSIMAL 3 GRUP\n》4 BULAN  Rp 25.000\nMAKSIMAL 2 GRUP + PREMIUM\n\nMAU NAMBAH GRUP LAGI \nPERMANEN Rp 25.000\nMAKSIMAL 2 GRUP'
